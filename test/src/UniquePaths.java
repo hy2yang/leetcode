b99dbb2e83fbb7aed0794a420d96d29ae2e68cd0
hy yang <almond2yang@gmail.com>
2017-10-15 18:12:33 -0700

    2 on oct 15

---
+++ test/src/UniquePaths.java
@@ -0,0 +1,27 @@
+
+public class UniquePaths {
+    
+    public static int uniquePaths(int m, int n) {
+        if (m==0 || n==0) return 0;
+        if (m==1 || n==1) return 1;
+        int l=Math.max(m, n);
+        int s=Math.min(m, n);
+        int[] swiper =new int[s];
+        for (int i=0;i<s;i++) {
+            swiper[i]=1;
+        }
+        
+        for (int i=1;i<l;i++) {
+            for (int j=1;j<s;j++) {
+                swiper[j]+=swiper[j-1];
+            }
+        }
+        return swiper[s-1];
+    }
+    
+    public static void main(String[] args) {
+        
+        System.out.print(uniquePaths(2,2));
+    }
+
+}
