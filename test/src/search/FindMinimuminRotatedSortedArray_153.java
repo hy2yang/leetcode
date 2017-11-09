407be12a71ccc2a969a2a3e1f17706f0e8415caa
hy yang <almond2yang@gmail.com>
2017-11-08 23:43:12 -0800

    Nov8th

---
+++ test/src/search/FindMinimuminRotatedSortedArray_153.java
@@ -0,0 +1,22 @@
+package search;
+
+public class FindMinimuminRotatedSortedArray_153 {
+    
+    public int findMin(int[] n) {
+        int lo=0,hi=n.length-1;                
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if (n[lo]<=n[mid] && n[mid]<=n[hi]) return n[lo];
+            if (n[lo]>n[mid]) {
+                hi=mid;
+                continue;
+            }
+            if (n[lo]>n[hi]) {
+                lo=mid+1;
+                continue;
+            }            
+        }
+        return n[lo];
+    }
+
+}
