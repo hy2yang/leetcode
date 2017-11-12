7f2a4db36044448db0117b1c93ae95063805111c
hy yang <almond2yang@gmail.com>
2017-11-12 15:33:28 -0800

    Nov12th

---
+++ test/src/general/RepeatedDNASequences_187.java
@@ -0,0 +1,23 @@
+package general;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+//Write a function to find all the 10-letter-long sequences (substrings) 
+//that occur more than once in a DNA molecule.
+
+public class RepeatedDNASequences_187 {
+    
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet<String> once= new HashSet<>();
+        HashSet<String> res= new HashSet<>();
+        for (int i=0;i<=s.length()-10;++i) {
+            String temp=s.substring(i, i+10);
+            if (once.contains(temp)) res.add(temp);
+            else once.add(temp);
+        }
+        return new ArrayList<String>(res);
+    }
+
+}
