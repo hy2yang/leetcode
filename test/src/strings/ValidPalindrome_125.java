70c022f0a967f81b6a4b6295796ea665b8211079
hy yang <almond2yang@gmail.com>
2017-11-14 14:32:56 -0800

    Nov13th

--- test/src/strings/ValidPalindrome_125.java
+++ test/src/strings/ValidPalindrome_125.java
@@ -1,4 +1,12 @@
 package strings;
+/*
+Given a string, determine if it is a palindrome, 
+considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+*/
 
 public class ValidPalindrome_125 {
     
