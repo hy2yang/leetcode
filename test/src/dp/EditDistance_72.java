c3fc198e7f1202a8ddae6c4df44f655a60e90d18
hy yang <almond2yang@gmail.com>
2017-11-30 14:39:37 -0800

    annotations

--- test/src/dp/EditDistance_72.java
+++ test/src/dp/EditDistance_72.java
@@ -35,5 +35,26 @@ public class EditDistance_72 {
      }
      return dp[b];
     }
+    
+    /*We define the state dp[i][j] to be the minimum number of operations to convert word1[0..i - 1] to word2[0..j - 1]. 
+    The state equations have two cases: the boundary case and the general case. 
+    Note that in the above notations, both i and j take values starting from 1.
+
+    For the boundary case: to convert a string to an empty string, 
+    the mininum number of operations to convert word1[0..i - 1] to "" requires at least i operations (deletions).
+    dp[i][0] = i;
+    dp[0][j] = j.
+            
+    The general case: convert a non-empty word1[0..i - 1] to another non-empty word2[0..j - 1].  
+    let's consider word[i - 1] and word2[j - 1]. 
+    If they are euqal, then no more operation is needed and dp[i][j] = dp[i - 1][j - 1].
+    
+    If they are not equal, we need to consider three cases:
+
+    Replace word1[i - 1] by word2[j - 1] (dp[i][j] = dp[i - 1][j - 1] + 1 (for replacement));
+    Delete word1[i - 1] and word1[0..i - 2] = word2[0..j - 1] (dp[i][j] = dp[i - 1][j] + 1 (for deletion));
+    Insert word2[j - 1] to word1[0..i - 1] and word1[0..i - 1] + word2[j - 1] = word2[0..j - 1] (dp[i][j] = dp[i][j - 1] + 1 (for insertion)).
+    For deletion,  convert word1[0..i - 2] to word2[0..j - 1], which costs dp[i - 1][j], and then deleting the word1[i - 1], which costs 1. 
+    The case is similar for insertion.*/
 
 }
--- test/src/dp/WordBreak_139.java
+++ test/src/dp/WordBreak_139.java
@@ -17,16 +17,16 @@ public class WordBreak_139 {
     
     public boolean wordBreak(String s, List<String> wordDict) {
         int len=s.length();
-        boolean[] startHere=new boolean[len+1];
-        startHere[0]=true;
+        boolean[] canBeStart=new boolean[len+1];    // i can be start <-> i=0 or 0~i-1 can be broken into dict words
+        canBeStart[0]=true;
         for (int i=0;i<len+1;++i) {        //  start
-            if (startHere[i]) {
+            if (canBeStart[i]) {
                 for (int j=i+1;j<len+1;++j) {    // end
-                    if (wordDict.contains(s.substring(i, j))) startHere[j]=true;
+                    if (wordDict.contains(s.substring(i, j))) canBeStart[j]=true;
                 }
             }            
         }
-        return startHere[len];         // len can be start <-> 0~len-1 part can be broken into dict words
+        return canBeStart[len];         // len can be start <-> 0~len-1 part can be broken into dict words
     }
 
 }
