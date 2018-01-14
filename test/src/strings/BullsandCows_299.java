73eb7990fb9605385c256ca65fa511eea37806ce
hy yang <almond2yang@gmail.com>
2018-01-13 17:41:32 -0800

    Jan13th

---
+++ test/src/strings/BullsandCows_299.java
@@ -0,0 +1,45 @@
+package strings;
+/*
+You are playing the following Bulls and Cows game with your friend: 
+You write down a number and ask your friend to guess what the number is. 
+Each time your friend makes a guess, you provide a hint that indicates 
+how many digits in said guess match your secret number exactly in both digit and position (called "bulls") 
+and how many digits match the secret number but locate in the wrong position (called "cows"). 
+Your friend will use successive guesses and hints to eventually derive the secret number.
+
+Write a function to return a hint according to the secret number and friend's guess, 
+use A to indicate the bulls and B to indicate the cows. In the above example, your function should return "1A3B".
+Please note that both secret number and friend's guess may contain duplicate digits:
+
+For example:
+
+Secret number:  "1807"
+Friend's guess: "7810"
+Hint: 1 bull and 3 cows. (The bull is 8, the cows are 0, 1 and 7.)
+
+Secret number:  "1123"
+Friend's guess: "0111"
+In this case, the 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow, and your function should return "1A1B".
+You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.
+*/
+public class BullsandCows_299 {
+    
+    public String getHint(String secret, String guess) {
+        int[] map=new int[10];
+        char[] s=secret.toCharArray();
+        char[] g=guess.toCharArray();
+        int a=0,b=0;
+        
+        for (int i=0;i<s.length;++i){    // if not exact match, use positive to record times of appearance in secret
+            if (s[i]==g[i]) ++a;         // use negtive to record times of appearance in guess
+            else {
+                if (map[s[i]-'0']<0) ++b; 
+                if (map[g[i]-'0']>0) ++b;
+                ++map[s[i]-'0'];
+                --map[g[i]-'0'];
+            }
+        }        
+        return a+"A"+b+"B";
+    }
+
+}
