fef59de720f59cb9821595b712ce9ab327d2dc80
hy2yang <frostsobi@live.com>
2018-02-19 19:49:49 -0800

    refactor Feb19th

--- test/src/linkedList/MergeTwoSortedLists_21.java
+++ test/src/linkedList/MergeTwoSortedLists_21.java
@@ -3,23 +3,25 @@ package linkedList;
 public class MergeTwoSortedLists_21 {
     
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1==null) return l2;
+    	if (l1==null) return l2;
         if (l2==null) return l1;
-        ListNode d=new ListNode(0);
-        d.next= l1.val<=l2.val? l1:l2;        
-        ListNode p=d.next;
-        if (p==l1) l1=l1.next;
-        else l2=l2.next;
+        ListNode d=new ListNode(0);              
+        ListNode cur=d;        
         
         while (l1!=null && l2!=null) {
-            p.next=l1.val<=l2.val? l1:l2;
-            p=p.next;
-            if (p==l1) l1=l1.next;
-            else l2=l2.next;            
+            if (l1.val<l2.val){
+                cur.next=l1;
+                l1=l1.next;
+            }
+            else {
+                cur.next=l2;
+                l2=l2.next;
+            } 
+            cur=cur.next;
         }
         
-        if (l2!=null) p.next=l2;
-        else p.next=l1;
+        if (l2==null) cur.next=l1;
+        else cur.next=l2;
         
         return d.next;
     }
