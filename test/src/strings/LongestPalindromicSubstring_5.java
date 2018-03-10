f34ebde63be092c7446f575943c58b328ef68049
hy yang <almond2yang@gmail.com>
2018-03-09 21:58:42 -0800

    Mar9th

---
+++ test/src/strings/LongestPalindromicSubstring_5.java
@@ -0,0 +1,30 @@
+package strings;
+
+public class LongestPalindromicSubstring_5 {
+    
+    public String longestPalindrome(String s) {
+        if (s.length()<2) return s;
+        char[] arr=s.toCharArray();
+        int maxlen=0;
+        int left=0;
+        int right=arr.length-1;
+        int lo,hi,next;
+        for(int i=0;i<arr.length-1;){
+            lo=i;hi=i;
+            while (hi+1<arr.length && arr[lo]==arr[hi+1]) ++hi;  
+            next=hi+1;
+            while (lo-1>=0 && hi+1<=arr.length-1 && arr[lo-1]==arr[hi+1]){
+                lo--;
+                hi++;
+            }
+            if (hi-lo+1>maxlen){
+                maxlen=hi-lo+1;
+                left=lo;
+                right=hi;
+            }
+            i=next;
+        }        
+        return s.substring(left, right+1);
+    } 
+
+}
