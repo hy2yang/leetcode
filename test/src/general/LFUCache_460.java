22f48d39d2c87b451e47f038dfb890390a9f194f
hy yang <almond2yang@gmail.com>
2018-08-27 21:33:00 -0700

    Aug27th

---
+++ test/src/general/LFUCache_460.java
@@ -0,0 +1,145 @@
+package general;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+/*
+Design and implement a data structure for Least Frequently Used (LFU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, 
+it should invalidate the least frequently used item before inserting a new item. 
+For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency), 
+the least recently used key would be evicted.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LFUCache cache = new LFUCache( 2 \/* capacity *\/ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.get(3);       // returns 3.
+cache.put(4, 4);    // evicts key 1.
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+*/
+
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+
+
+public class LFUCache_460 {
+	
+	private class LfuNode{
+		int key;
+		int value;
+		int f;
+		
+		LfuNode(int k, int v){	
+			this.key = k;
+			this.value = v;
+			this.f = 0;
+		}
+	}
+	
+	private int capacity;
+	private int minF;
+	private Map<Integer, LfuNode> m ;
+	private TreeMap<Integer, LinkedHashSet<Integer>> fMap;
+	
+	public LFUCache_460(int capacity) {
+		this.capacity = capacity;
+		minF = 0;		
+		m = new HashMap<>();
+		fMap = new TreeMap<>();
+	}
+
+	public int get(int key) {
+		if (m.containsKey(key)) {
+			LfuNode cur = m.get(key);
+			promote(cur);
+			return cur.value;
+		}
+		return -1;
+	}
+
+	public void put(int key, int value) {
+        if (capacity <= 0) return;
+		
+		if (m.containsKey(key)) {
+			LfuNode cur = m.get(key);
+			cur.value = value;
+            promote(cur);
+		} 
+		else {
+            if (m.size()==capacity) {
+			    this.invalidate();
+		    }
+			LfuNode cur = new LfuNode(key, value);
+			minF = 0;
+            m.put(key, cur);
+		    putIntoFMap(cur);
+		}
+	}
+	
+	private void putIntoFMap(LfuNode cur) {		
+		LinkedHashSet<Integer> temp = fMap.getOrDefault(cur.f, new LinkedHashSet<>());
+		temp.add(cur.key);
+		fMap.put(cur.f, temp);		
+	}
+	
+	private void promote(LfuNode cur) {
+		int freq = cur.f;
+
+		Set<Integer> thisFSet = fMap.get(freq);
+		if (thisFSet != null) {
+			thisFSet.remove(cur.key);
+			if (thisFSet.isEmpty())
+				fMap.remove(freq);				
+		}
+		++cur.f;		
+		putIntoFMap(cur);
+        minF = fMap.firstKey();
+	}
+	
+	private void invalidate() {
+		LinkedHashSet<Integer> minFSet = fMap.get(minF);
+		int key = minFSet.iterator().next();
+		minFSet.remove(key);
+		m.remove(key);
+		if (minFSet.isEmpty()) {
+			fMap.remove(minF);
+			minF = fMap.isEmpty()? 0:fMap.firstKey();
+		}
+	}
+	
+	public static void main(String[] args) {
+		LFUCache_460 obj = new LFUCache_460(2);
+		obj.put(1, 1);
+		obj.put(2, 2);
+		System.out.println(obj.get(1));
+		obj.put(3, 3);
+		System.out.println(obj.get(2));
+		System.out.println(obj.get(3));
+		obj.put(4, 4);
+		System.out.println(obj.get(1));
+		System.out.println(obj.get(3));
+		System.out.println(obj.get(4));
+	}
+
+}
