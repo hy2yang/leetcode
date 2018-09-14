66ffa4486073e99ca5389cd696b5f25683572fbb
hy yang <almond2yang@gmail.com>
2018-09-14 10:58:00 -0700

    array stack

--- src/general/ValidParentheses_20.java
+++ src/general/ValidParentheses_20.java
@@ -11,6 +11,22 @@ public class ValidParentheses_20 {
 
     public boolean isValid(String s) {
         if (s==null || s.length()<1) return true;
+        if ((s.length()&1)!=0) return false;
+        char[] cs=s.toCharArray();
+        char[] stack = new char[cs.length];
+        int p = 0;
+
+        for (char c:cs) {
+            if (c=='(') stack[p++]=')';
+            else if (c=='[') stack[p++]=']';
+            else if (c=='{') stack[p++]='}';
+            else if (p==0 || stack[--p]!=c) return false;
+        }
+        return p==0;
+    }
+
+    public boolean isValid_actualStack(String s) {
+        if (s==null || s.length()<1) return true;
         if (s.length()%2==1) return false;
         char[] cs=s.toCharArray();
         ArrayDeque<Character> stack=new ArrayDeque<>(); // stack contains right ones to be matched
